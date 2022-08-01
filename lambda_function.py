7438393d4b5116ed2566f26f6e2618c095a2a2dd
ravgondi <107649214+ravgondi@users.noreply.github.com>
2022-08-01 15:48:45 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -21,23 +21,23 @@ def lambda_handler(event, context):
     csv_object = s3.Object(bucket,key)
     csv_file = csv_object.get()['Body'].read().decode('utf-8')
     f = io.StringIO(csv_file)
-    reader = csv.DictReader(f, ('ID', 'Status', 'Source Server Name','Target Server Name','Target Server VM Size', 'Target Account', 'Target Availablity Zone','Target VPC', 'Target Subnet','EBS type', 'Tags', 'EC2 Instance Profile','Security Groups'))
+    reader = csv.DictReader(f, ('ID', 'Status', 'Source Server Name','Target Server Name','Target Server VM Size', 'Target Account', 'Target Availablity Zone','Target VPC', 'Target Subnet','EBS type','Tags','EC2 Instance Profile','Security Groups'))
     list_of_json = [dict(device) for device in reader]
+    print (list_of_json)
     f.close()
     #gets list of source server ids and maps to hostname
     lt_hostname_map = map_lt_to_hostname(get_source_server_ids())
-    print ("lt_hostname_map")
-    print (lt_hostname_map)
     #gets narrowed lists (no repeats, removes unnecessary fields, and changes certain field names)
     lt_narrowed_list = get_lt_narrowed_list(list_of_json[1:])
-    print ("lt_narrowed_list")
-    print (lt_narrowed_list)
     combined_map = combined_dict_list(lt_narrowed_list,lt_hostname_map)
-    print ("combined map")
-    print (combined_map)
     response = modify_lts(combined_map)
-   
     return(response)
+
+#convert bytes
+def bytesto(bytes, to, bsize=1024):
+    a = {'k' : 1, 'm': 2, 'g' : 3, 't' : 4, 'p' : 5, 'e' : 6 }
+    r = float(bytes)
+    return bytes / (bsize ** a[to])
  
 #union the two dicts where the hostname in each is the same
 def combined_dict_list(narrowed_list,lt_hostname_map):
@@ -56,57 +56,42 @@ def combined_dict_list(narrowed_list,lt_hostname_map):
 def get_source_server_ids():
     source_server_list=[]
     response = mgn.describe_source_servers(filters={})
+    print (response)
     for i in response['items']:
         source_server_dict={}
         source_server_dict['sourceServerID'] =i['sourceServerID']
         source_server_dict['hostname'] = i['sourceProperties']['identificationHints']['hostname'].split('.',1)[0]
         source_server_dict['disks'] = i['dataReplicationInfo']['replicatedDisks']
         source_server_list.append(source_server_dict.copy())
-        if i['dataReplicationInfo']['dataReplicationState'] == 'DISCONNECTED' or 'CUTOVER' or 'DISCOVERED':
-            continue
-        else:
-            if i['Operating System'] == 'Windows':
+        if i['dataReplicationInfo']['dataReplicationState'] not in ['DISCONNECTED' or 'CUTOVER' or 'DISCOVERED']:
                 response = mgn.update_launch_configuration(
-                    #bootMode='LEGACY_BIOS'|'UEFI',
-                    #copyPrivateIp=True|False,
-                    copyTags=True,
-                    #launchDisposition='STOPPED'|'STARTED',
-                    licensing={
-                        'osByol': False
-                    },
-                    #name='string',
-                    sourceServerID=i['sourceServerID'],
-                    targetInstanceTypeRightSizingMethod='NONE'
-                )   
-            else:
-                response = mgn.update_launch_configuration(
-                    #bootMode='LEGACY_BIOS'|'UEFI',
-                    #copyPrivateIp=True|False,
-                    copyTags=True,
-                    #launchDisposition='STOPPED'|'STARTED',
-                    licensing={
-                        'osByol': True
-                    },
-                    #name='string',
-                    sourceServerID=i['sourceServerID'],
-                    targetInstanceTypeRightSizingMethod='NONE'
-                )   
+                #bootMode='LEGACY_BIOS'|'UEFI',
+                #copyPrivateIp=True|False,
+                copyTags=True,
+                #launchDisposition='STOPPED'|'STARTED',
+                #licensing={
+                #    'osByol': False
+                #    },
+                #name='string',
+                sourceServerID=i['sourceServerID'],
+                targetInstanceTypeRightSizingMethod='NONE'
+                )
     return(source_server_list)
 def get_lt_narrowed_list(lt_list):
     narrowed_lt_list =[]
     for i in lt_list:
-        tags = i['Tags'].replace(';',',')
-        output = [{"Key": k, "Value": v} for k, v in json.loads(tags).items()]
+        #tags = i['Tags'].replace(';',',')
+        #output = [{"Key": k, "Value": v} for k, v in json.loads(tags).items()]
         temp_dict={}
         temp_dict['Target Server VM Size'] = i['Target Server VM Size']
         temp_dict['Target Subnet'] = i['Target Subnet']
         temp_dict['Target VPC'] = i['Target VPC']
         temp_dict['Tags'] = i['Tags']
         temp_dict['Target Account'] = i['Target Account']
-        temp_dict['EC2 Instance Profile'] = i['EC2 Instance Profile']
+        #temp_dict['EC2 Instance Profile'] = i['EC2 Instance Profile']
         temp_dict['hostname'] = i['Target Server Name'].split('.',1)[0]
         temp_dict['Security Groups'] = i['Security Groups'].replace(';',',').split(",")
-        temp_dict['Tags'] = output
+        #temp_dict['Tags'] = output
         narrowed_lt_list.append(temp_dict)
     return narrowed_lt_list
    
@@ -114,10 +99,7 @@ def map_lt_to_hostname(ss_dict):
     hostname_lt_map_list=[]
     for i in ss_dict:
         hostname_lt_map_dict={}
-        response = mgn.get_launch_configuration(
- 
-            sourceServerID=i['sourceServerID']
-        )
+        response = mgn.get_launch_configuration(sourceServerID=i['sourceServerID'])
         if 'ec2LaunchTemplateID' in response:
             hostname_lt_map_dict['ec2LaunchTemplateID'] = response['ec2LaunchTemplateID']
             hostname_lt_map_dict['hostname'] = i['hostname']
@@ -128,6 +110,7 @@ def map_lt_to_hostname(ss_dict):
 def modify_lts(lt_list):
  
     for i in lt_list:
+        print (i)
         disk_list = []
         for j in i['disks']:
             if 'deviceName' in j:
@@ -135,18 +118,19 @@ def modify_lts(lt_list):
                 disk_dict['DeviceName'] = j['deviceName']
                 disk_dict['VirtualName'] = j['deviceName']
                 disk_dict['Ebs'] = {}
-                disk_dict['Ebs']['Encrypted'] = True
+                #disk_dict['Ebs']['Encrypted'] = True
                 disk_dict['Ebs']['VolumeSize']= int(bytesto(j['totalStorageBytes'],'g'))
+                disk_dict['Ebs']['DeleteOnTermination'] = True
                 disk_dict['Ebs']['VolumeType'] = 'gp3'
                 disk_list.append(disk_dict.copy())
  
         response = ec2.create_launch_template_version(
             LaunchTemplateId=i['ec2LaunchTemplateID'],
-            VersionDescription='string',
+            VersionDescription='UpdatedByLambdaFunction',
             LaunchTemplateData={
                 'EbsOptimized': True,
                 'IamInstanceProfile': {
-                    'Arn': "arn:aws:iam::" + i['Target Account'] + ":instance-profile/" + i["EC2 Instance Profile"]
+                   'Arn': "arn:aws:iam::" + i['Target Account'] + ":instance-profile/" + i["EC2 Instance Profile"]
                 },
                 'BlockDeviceMappings': disk_list,
                 'NetworkInterfaces': [
@@ -156,14 +140,13 @@ def modify_lts(lt_list):
                         'DeleteOnTermination': True,
                         'DeviceIndex': 0,
                         'Groups': i['Security Groups'],
+                        'SubnetId': i['Target Subnet'],
                     }
                 ],
                 #'ImageId': i['AMI ID'],
                 'InstanceType': i['Target Server VM Size'],
-                #'KeyName': key_name,
-                'Monitoring': {
-                    'Enabled': True
-                },
+                'KeyName': key_name,
+                'Monitoring': {'Enabled': True},
                 'TagSpecifications': [
                     {
                         'ResourceType': 'instance',
@@ -181,7 +164,7 @@ def modify_lts(lt_list):
                     }
                 ],
  
-                'DisableApiTermination': True,
+                'DisableApiTermination': False,
             }
         )
  
